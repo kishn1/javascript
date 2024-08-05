d50682b16bfce4ce97d382bfaeee3eab4ea012f7
kishn1 <147910728+kishn1@users.noreply.github.com>
2024-08-05 17:01:02 +0000

    zomato

---
+++ zomato/test.js
@@ -0,0 +1,41 @@
+/*maan lo mai ek zomato jaisi ek website
+ bna rha hu to mujhe kis bhi user ki ditels 
+ chahiye jaise account id password extra*/
+
+
+const accountId = 9026464822  // const keybord likhne ke baad ham use chang nhi kar sakte hai
+let accountEmail = "kishanddu01@gmail.com"
+var accountPassword = "12345"      //ham jyada tar use let karte hai kyuki var me scope{} vala problam hota hai ek jagah change karne se har jgah ho jata hai
+accountCity = "iliya"
+let accountState;        // agar aap koi variable bna ke ushe khale chhod dete hia tab uski value undifaine aata hai
+
+console.log(accountPassword);
+
+//const ko chhod ke sabhi ko chang kiya ja skta hai jaise 
+
+accountEmail = "kk6455917@gmail.com"
+accountPassword = "902646"
+accountCity = "chakiya"
+
+//en sbko ek sath print karne ke liye condole.table([]) ka use kiya jata hai
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+/***********DETATYPES IN JS */
+//**PRIMITIVE DETATYPES */
+
+// number=>2 hogya power
+//bigint=> bahot bada number iskya jyadetar use treding me kiya jaata hai
+//string=>""
+//boolean=>true/fals---jaise koi user login hai ya nhi hai hai
+//null=> standalone value ---matalb ki mt khali
+//undifined=> matlab ki bas koi variabe declear kiya gya ho
+//symbol=>unique
+
+//***NOMPRIMITIVE DATA TYPES */
+
+//Object=>
+//fanction=>
+
+
